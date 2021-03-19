c683abaea7a9ba56b1010ed66860fbee901767a3
juho kalberg <juhokalberg@juhos-MacBook-Pro.local>
2021-03-19 17:22:14 +0200

    write database function

--- tund_02_show_news.php
+++ tund_02_show_news.php
@@ -14,9 +14,24 @@
 		echo $conn -> error;
 		$stmt -> bind_result($news_title_from_db, $news_content_from_db, $news_author_from_db);
 		$stmt -> execute();
+		$raw_news_html = null;
+		while ($stmt -> fetch()) {
+			$raw_news_html .= "\n <h2>" .$news_title_from_db ."</h2>";
+			$raw_news_html .= "\n <p>" .nl2br($news_content_from_db) ."</p>";
+			$raw_news_html .= "\n <p>Edastas: ";
+			if(!empty($news_author_from_db)) {
+				$raw_news_html .= $news_author_from_db;
+			} else {
+				$raw_news_html .= "Tundmatu reporter";
+			}
+			$raw_news_html .= "</p>";
+		};
 		$stmt -> close();
 		$conn -> close();
+		return $raw_news_html;
 	}
+
+	$news_html = read_news();
 
 ?>
 <!DOCTYPE html>
