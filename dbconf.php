cfede364320ff926ee43832a9c80322568b38b98
Juho Kalberg <juss@vilenkell.eu>
2021-04-22 16:52:45 +0300

    Lisatud eraldu 03_tund

--- dbconf.php
+++ dbconf.php
@@ -2,6 +2,6 @@
 //see fail on andmebaasi kaasamiseks ja üjes kohas muutmiseks
 // require_once "conf.php";    //kui kasutad lokaalses serveris, siis aadressiks:
                             //"conf.php",
-require_once "../../conf.php"; 
+require_once "../conf.php"; 
                             //sest asub siinsamas root kataloogis kui tigus, siis on seal asuv
                             //"../../conf.php"
