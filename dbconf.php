917c603cc3949525056d2cfac94f550e39dfbc0d
Juho Kalberg <juss@vilenkell.eu>
2021-04-22 17:03:40 +0300

    Lisatud eraldu 03_tund

--- dbconf.php
+++ dbconf.php
@@ -2,7 +2,7 @@
 //see fail on andmebaasi kaasamiseks ja üjes kohas muutmiseks
 // require_once "conf.php";    //kui kasutad lokaalses serveris, siis aadressiks:
                             //"conf.php",
-include("../test.php");     // just for testing, if file exists, it hows up.
+include("../../test.php");     // just for testing, if file exists, it hows up.
 require_once "../conf.php"; 
                             //sest asub siinsamas root kataloogis kui tigus, siis on seal asuv
                             //"../../conf.php"
