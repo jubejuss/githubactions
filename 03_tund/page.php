082f7c3aad0bcb3a6a56e2528e697fec862efd73
Juho Kalberg <juss@vilenkell.eu>
2021-04-22 16:31:35 +0300

    Lisatud eraldu 03_tund

--- 03_tund/page.php
+++ 03_tund/page.php
@@ -1,4 +1,7 @@
 <?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', TRUE);
+	ini_set('display_startup_errors', TRUE);
 	//session_start();
 	SessionManager::sessionStart("vr", 0, "/~juho.kalberg/", "tigu.hk.tlu.ee");
 
