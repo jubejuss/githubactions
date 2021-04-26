e95e0bca3a5f3b70d6aa10b803cd0f104febad22
Juho Kalberg <juss@vilenkell.eu>
2021-04-26 13:04:07 +0300

    gitignore dbconf.php

--- dbconf.php
+++ dbconf.php
@@ -1,8 +1,2 @@
 <?php
-//see fail on andmebaasi kaasamiseks ja üjes kohas muutmiseks
-require_once "conf.php";    //kui kasutad lokaalses serveris, siis aadressiks:
-                            //"conf.php",
-/* include("../../../test.php");     // just for testing, if file exists, it hows up.
-require_once "../../../conf.php"; 
-                            //sest asub siinsamas root kataloogis kui tigus, siis on seal asuv
-                            //"../../conf.php"*/
+    require_once "conf.php";
