852a1acdf9c447f4a99d939a156e5f0e2b487c76
Juho Kalberg <juss@vilenkell.eu>
2021-05-01 13:13:35 +0300

    test

--- andruseversioonid/fnc_general.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function test_input($data) {
-		$data = filter_var($data, FILTER_SANITIZE_STRING);
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
