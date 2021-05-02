36bb4cb766d1b8229f14d4391c7c99a940355f7d
Juho Kalberg <juss@vilenkell.eu>
2021-05-02 12:34:39 +0300

    new index page

--- conf.php
+++ conf.php
@@ -1,5 +1,4 @@
 <?php
-
 	$server_user_name = "root";
 	$server_password = "root";
 	$server_host = "db:3306";
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="et">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/starter.css" />
+    <title>Veebirakenduste loomise tundide kodu</title>
+  </head>
+
+  <body>
+    <div class="col-md-8 mx-auto">
+      <h1>Tunnitööd ja kodutööd</h1>
+      <a href="page.php">Pildi- ja uudislehe meisterdamine</a><br />
+    </div>
+
+    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script type="module" src="assets/js/starter.js"></script>
+  </body>
+</html>
