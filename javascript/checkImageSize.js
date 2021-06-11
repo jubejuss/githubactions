89176e40b7f44d0eae356767ba9b09017aafb543
Juho Kalberg <juss@vilenkell.eu>
2021-06-11 10:40:39 +0300

    added javascript file

--- javascript/checkImageSize.js
+++ javascript/checkImageSize.js
@@ -1,5 +1,10 @@
 let fileSizeLimit = 1 * 1024 * 1024;
 
-window onload = function () {
-    document getElementById("Photo_submit").disabled = true;
+window.onload = function () {
+    document.getElementById("Photo_submit").disabled = true;
+    document.getElementById("file_input").addEventListener("change", checkSize);
+}
+
+function checkSize() {
+
 }
