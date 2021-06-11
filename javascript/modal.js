5517709a9dc930a89e674e96d7d2d7401c73f70c
Juho Kalberg <juss@vilenkell.eu>
2021-06-11 12:25:00 +0300

    add rating AJAX

--- javascript/modal.js
+++ javascript/modal.js
@@ -19,11 +19,41 @@ window.onload = function () {
 
 function openModal(e) {
     modalImg.src = photoDir + e.target.dataset.fn; //see fn oli meil endal html-i pandud, nüüd siin kasutan
+    photoId = e.target.dataset.id;
     captionText.innerHTML = e.target.alt;
+    //nullin hinde osa
+    document.getElementsByTagName("avgRating").innerHTML = "";
+    for (let i = 1; i < 6; i++) {
+        document.getElementById("rate" + i).checked = false;
+    }
+    document.getElementById("storeRating").addEventListener("click", storeRating);
     modal.style.display = "block";
 }
 
 function closeModal() {
+    document.getElementById("modalclose").removeEventListener("click", closeModal);
+    document.getElementById("storeRating").removeEventListener("click", storeRating);
     modal.style.display = "none";
     modalImg.src = "images/empty.png";
+}
+
+function storeRating() {
+    let rating = 0;
+    for (let i = 1; i < 6; i++) {
+        if (document.getElementById("rate" + i).checked) {
+            rating = i;
+        }
+        if (rating > 0) {
+            //AJAX
+            let webRequest = new XMLHttpRequest();
+            webRequest.onreadystatechange = function () {
+                //kas õnnestus
+                if (this.readyState == 4 && this.status == 200) {
+                    //mida teeme kui õnnestus
+                    document.getElementById("avgRating").innerHTML = "Keskmine hinne: " + this.responseText;
+                }
+            }
+            //AJAX lõppeb
+        }
+    }
 }
